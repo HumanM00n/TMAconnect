bc03e17ba501820aa3f931213cf5a74ebc6b2fba
Mathis ROUY <c1120@cnetp.fr>
2023-07-25 10:56:57 +0200

    test

--- test-code.php
+++ test-code.php
@@ -4,42 +4,7 @@ $password = "XVsikn92";
 $dbname = "tmaconnect";
 $error = false;
 $dsn = "mysql:host=localhost:3308;dbname=tmaconnect";
-
-if (isset($_POST['login'])) {
-  // Vérifier les informations de connexion (par exemple, vérifier dans la base de données)
-  $matricule = $_POST['username'];
-  $passwd = $_POST['password'];
-
-  try {
-    $pdo = new PDO($dsn, $username, $password);
-
-    $selectQuery = "SELECT passwd FROM tc_utilisateur WHERE matricule = :matricule";
-    $selectStmt = $pdo->prepare($selectQuery);
-    $selectStmt->execute(['matricule' => $matricule]);
-    $row = $selectStmt->fetch();
-
-    if (!empty($row) && password_verify($passwd, $row['passwd'])) {
-      // L'utilisateur est authentifié avec succès
-
-      if (isset($_POST['remember']) && $_POST['remember'] === 'on') {
-        // Si l'utilisateur a coché "Rester Connecté", créer un cookie avec une durée de validité plus longue (par exemple, 30 jours)
-        $cookieValue = uniqid(); // Vous pouvez générer un identifiant unique pour l'utilisateur
-        setcookie('remember_token', $cookieValue, time() + (30 * 24 * 60 * 60)); // Le cookie expirera dans 30 jours
-      }
-
-      // Rediriger vers la page de profil ou le tableau de bord de l'utilisateur
-      // header('Location: profil.php');
-      // exit;
-    } else {
-      // Les informations d'authentification sont invalides
-      echo 'Nom d\'utilisateur ou mot de passe incorrect.';
-    }
-  } catch (PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
-  }
-}
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -57,77 +22,21 @@ if (isset($_POST['login'])) {
 </header>
 
 <body>
-  <div class="container">
-    <h2>
-      <center>Connexion à votre compte</center>
-    </h2>
-    <?php
-    if (isset($error) && $error) {
+  <?php
+  $semaine = array();
+  $semaine['premier_jour'] = 'lundi';
+  $semaine['deuxième_jour'] = 'mardi';
+  $semaine['troisième_jour'] = 'mercredi';
+  $semaine['quatrième_jour'] = 'jeudi';
+  $semaine['cinquième_jour'] = 'vendredi';
+  $semaine['sixième_jour'] = 'samedi';
+  $semaine['septième_jour'] = 'dimanche';
 
-      echo '<center><p class="error--id">Identifiant ou mot de passe incorrecte</p></center>';
-    }
-    ?>
-
-    <!-- CHAMPS DE FORMULAIRE -->
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-      <section class="bloc_id">
-        <div class="identifiant">
-          <p>Identifiant</p>
-          <input class="champs_txt" type="text" name="username" required minlength="5" max="5">
-          <span class="icon">
-            <ion-icon name="person"></ion-icon>
-          </span>
-        </div>
-      </section>
-
-      <div class="lmdp">
-        <a href="../TMAconnect/change-mdp.php" class="tma-btn">Mot de passe oublié</a>
-      </div>
-
-      <section class="bloc_mdp">
-        <div class="mdp">
-          <label>
-            <p>Mot de passe</p>
-            <input class="champs_txt" type="password" name="password" required minlength="" maxlength="30">
-
-            <!-- ICONES -->
-            <div class="password-icon">
-              <i data-feather="eye"></i>
-              <i data-feather="eye-off"></i>
-            </div>
-          </label>
-
-          <script src="https://unpkg.com/feather-icons"></script>
-          <script>
-            feather.replace();
-          </script>
-
-          <script>
-            const eye = document.querySelector('.feather-eye');
-            const eyeoff = document.querySelector(' .feather-eye-off');
-            const passwordField = document.querySelector('input[type=password]');
-
-            eye.addEventListener('click', () => {
-              eye.style.display = "none";
-              eyeoff.style.display = "block";
-              passwordField.type = "text";
-            });
-
-            eyeoff.addEventListener('click', () => {
-              eyeoff.style.display = "none";
-              eye.style.display = "block";
-              passwordField.type = "password";
-            });
-          </script>
-        </div>
-        <a href="../TMAconnect/home.php"><input class="tma-btn" type="submit" value="Se Connecter"></a>
-      </section>
-    </form>
-    <label class="remember"><input type="checkbox">Se souvenir de moi</label>
-  </div>
-  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-
-
+  echo $semaine['premier_jour'], $semaine['deuxième_jour'] = 'mardi', $semaine['quatrième_jour'] = 'jeudi',
+  $semaine['cinquième_jour'] = 'vendredi',
+  $semaine['sixième_jour'] = 'samedi',
+  $semaine['septième_jour'] = 'dimanche';
+  ?>
 
 </body>
 
