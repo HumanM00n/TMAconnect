2149a2d338fbf82a69da785818733c168abf8be6
Mathis ROUY <c1120@cnetp.fr>
2023-07-28 15:22:26 +0200

    test

--- test-code.php
+++ test-code.php
@@ -1,199 +1,76 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this template
+-->
 
-// informations de connexion à la base de données MySQL
+<?php
+// Informations de connexion � la base de donn�es MySQL
 $servername = "localhost:3308"; // nom du serveur
 $username = "root"; // nom d'utilisateur
 $password = "XVsikn92"; // mot de passe
-$dbname = "tmaconnect"; // nom de la base de données
-// création d'une connexion à la base de données
+$dbname = "tmaconnect"; // nom de la base de donn�es
+// Cr�ation d'une connexion � la base de donn�es avec PDO
+$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-
-try {
-  $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  echo "La connexion a �chou� : " . $e->getMessage();
-}
-
-if (isset($_POST["username"]) && isset($_POST["password"])) {
-  $st = $bdd->prepare("SELECT passwd FROM tc_utilisateur WHERE matricule=:username");
-  $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-  $st->bindParam(':username', $username);
-
-  if ($st->execute()) {
-    $hashedPassword = $st->fetchColumn();
-
-    if ($hashedPassword && password_verify($_POST["password"], $hashedPassword)) {
-      $username1 = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-      $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-
-      $_SESSION['username'] = $username1;
-      $_SESSION['password'] = $password;
-
-      $st1 = $bdd->prepare("UPDATE tc_utilisateur SET derniere_connect = now() WHERE matricule=:username");
-      $st1->bindParam(':username', $_POST["username"]);
-      $st1->execute();
-
-      header("Location: http://localhost/TMAconnect/home.php");
-      exit();
-    } else {
-      $error = true;
-    }
-  } else {
-    $error = true;
-  }
-}
+// Configuration des attributs de PDO
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
+  <title>TMA - Affichage des demandes</title>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/test-css.css">
-  <link rel="icon" href="../TMAconnect/img/NLogo2.png" />
-  <title>TMAconnect - test-code</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="css/csshome.css" rel="stylesheet" type="text/css"/>
+  <link href="css/test-css.css" rel="stylesheet" type="text/css"/>
+  <link rel="icon" href="img/NLogo2.png"/>
 </head>
-<header>
-  <center><img class="logo" src="img/NLogo.png"></center>
-</header>
 
 <body>
+  <?php
+  include('includes/header.html.inc.php');
+  ?>
 
-  <div class="container">
-    <h2>
-      <center>Connexion à votre compte</center>
-    </h2>
-    <?php
-    if (isset($error) && $error) {
-
-      echo '<center><p class="error--id">Identifiant ou mot de passe incorrecte</p></center>';
-    }
-    ?>
-
-    <!-- CHAMPS DE FORMULAIRE -->
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-      <section class="bloc_id">
-        <div class="identifiant">
-          <p>Identifiant</p>
-          <input class="champs_txt" type="text" name="username" required minlength="5" max="5">
-          <span class="icon">
-            <ion-icon name="person"></ion-icon>
-          </span>
-        </div>
-      </section>
-
-      <div class="lmdp">
-        <a href="./change-mdp.php" class="tma-btn">Mot de passe oublié</a>
+<section id="modif">
+  <form class="formmodif" name="formmodif" action="" method="POST">
+    <fieldset id="infos">
+      <legend>Filtres</legend>
+      <div>
+        <label for="accèsDemande">Accès Directe à la Demande</label>
+        <input type="text" size="35">
+      </div>
+      <!-- Nouveaux champs de texte -->
+      <div>
+        <label for="">Contenant du texte</label>
+        <input type="text" size="35">
       </div>
 
-      <section class="bloc_mdp">
-        <div class="mdp">
-          <label>
-            <p>Mot de passe</p>
-            <input class="champs_txt" type="password" name="password" required minlength="" maxlength="30">
+      <!-- Nouvelles listes déroulantes -->
+      <div>
+        <label for="select_departement">Domaine</label>
+        <select>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+          <!-- Ajoutez d'autres options selon vos besoins -->
+        </select>
+      </div>
+      <div>
+        <label for="select_poste">Demandes du groupe</label>
+        <select name="select_poste" id="select_poste">
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+          <!-- Ajoutez d'autres options selon vos besoins -->
+        </select>
+      </div>
+    </fieldset>
+  </form>
+</section>
 
-            <!-- ICONES -->
-            <div class="password-icon">
-              <i data-feather="eye"></i>
-              <i data-feather="eye-off"></i>
-            </div>
-          </label>
-
-          <script src="https://unpkg.com/feather-icons"></script>
-          <script>
-            feather.replace();
-          </script>
-
-          <script>
-            const eye = document.querySelector('.feather-eye');
-            const eyeoff = document.querySelector(' .feather-eye-off');
-            const passwordField = document.querySelector('input[type=password]');
-
-            eye.addEventListener('click', () => {
-              eye.style.display = "none";
-              eyeoff.style.display = "block";
-              passwordField.type = "text";
-            });
-
-            eyeoff.addEventListener('click', () => {
-              eyeoff.style.display = "none";
-              eye.style.display = "block";
-              passwordField.type = "password";
-            });
-          </script>
-        </div>
-      </section>
-      <label>
-        <input type="checkbox" name="remember" value="1" />
-        Se souvenir de moi
-      </label>
-      <a href="./home.php"><input class="tma-btn" type="submit" value="Se Connecter"></a>
-    </form>
-
-  </div>
-  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 
 </body>
 
 </html>
-
-
-
-
-
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="css/test-css.css" <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  <title>T-Bootstrap</title>
-</head>
-
-<body>
-  <-- Formulaire de connexion -->
-<!-- <form action="test-code.php" method="POST">
-    <label for="username">Nom d'utilisateur:</label>
-    <input type="text" type="text" name="username" required minlength="5" max="5">
-
-    <label for="password">Mot de passe:</label>
-    <input type="password" type="password" name="password" required minlength="4" maxlength="30">
-
-    <button type="submit">Se connecter</button>
-  </form>
-
-  <button type="button" class="btn btn-primary">Primary</button>
-  <a href="#" class="btn btn-primary disabled" tabindex="-1" role="button" aria-disabled="true">Primary link</a>
-  <a href="#" class="btn btn-secondary disabled" tabindex="-1" role="button" aria-disabled="true">Link</a>
-
-</body>
-
-</html>  -->
-
-
-
-<?php
-// $semaine = array();
-// $semaine['premier_jour'] = 'lundi';
-// $semaine['deuxième_jour'] = 'mardi';
-// $semaine['troisième_jour'] = 'mercredi';
-// $semaine['quatrième_jour'] = 'jeudi';
-// $semaine['cinquième_jour'] = 'vendredi';
-// $semaine['sixième_jour'] = 'samedi';
-// $semaine['septième_jour'] = 'dimanche';
-
-// echo $semaine['premier_jour'], $semaine['deuxième_jour'] = 'mardi', $semaine['quatrième_jour'] = 'jeudi',
-// $semaine['cinquième_jour'] = 'vendredi',
-// $semaine['sixième_jour'] = 'samedi',
-// $semaine['septième_jour'] = 'dimanche';
-?>
