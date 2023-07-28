8f4f70e0dd1593117264e4d632a8d73df6ba7f04
Mathis ROUY <c1120@cnetp.fr>
2023-07-28 11:46:46 +0200

    Ajout du tableau des demandes

--- demande/AffichageDemande.php
+++ demande/AffichageDemande.php
@@ -3,27 +3,67 @@
 Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this template
 -->
+
+<?php
+// Informations de connexion � la base de donn�es MySQL
+$servername = "localhost:3308"; // nom du serveur
+$username = "root"; // nom d'utilisateur
+$password = "XVsikn92"; // mot de passe
+$dbname = "tmaconnect"; // nom de la base de donn�es
+// Cr�ation d'une connexion � la base de donn�es avec PDO
+$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+// Configuration des attributs de PDO
+?>
+
 <html>
 
 <head>
-    <title>TMA - Création Demande</title>
+    <title>TMA - Affichage des demandes</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href="../css/AffichageDemande.css" rel="stylesheet" type="text/css" />
+    <link href="../css/csshome.css" rel="stylesheet" type="text/css" />
     <link rel="icon" href="../img/NLogo2.png" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://unpkg.com/file-saver/dist/FileSaver.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
     <?php
     include('../includes/header.html.inc.php');
+    ?>
 
-    // Informations de connexion � la base de donn�es MySQL
-    $servername = "localhost:3308"; // nom du serveur
-    $username = "root"; // nom d'utilisateur
-    $password = "XVsikn92"; // mot de passe
-    $dbname = "tmaconnect"; // nom de la base de donn�es
-    // Cr�ation d'une connexion � la base de donn�es avec PDO
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
-    // Configuration des attributs de PDO
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
