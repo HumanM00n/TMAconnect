6b918907d5f823082c1a03cd12151c5af9bbc5d2
Mathis ROUY <c1120@cnetp.fr>
2023-08-23 16:34:26 +0200

    Test effectué sur la page

--- test-code.php
+++ test-code.php
@@ -11,9 +11,9 @@ $username = "root"; // nom d'utilisateur
 $password = "XVsikn92"; // mot de passe
 $dbname = "tmaconnect"; // nom de la base de donn�es
 // Cr�ation d'une connexion � la base de donn�es avec PDO
-$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
 // Configuration des attributs de PDO
+$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 ?>
 
 <html>
@@ -23,54 +23,51 @@ $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <link href="css/csshome.css" rel="stylesheet" type="text/css"/>
-  <link href="css/test-css.css" rel="stylesheet" type="text/css"/>
-  <link rel="icon" href="img/NLogo2.png"/>
+  <link href="css/csshome.css" rel="stylesheet" type="text/css" />
+  <link href="css/test-css.css" rel="stylesheet" type="text/css" />
+  <link rel="icon" href="img/NLogo2.png" />
 </head>
 
 <body>
   <?php
   include('includes/header.html.inc.php');
   ?>
+  <section id="modif">
+    <form class="formmodif" name="formmodif" action="" method="POST">
+      <fieldset id="infos">
+        <legend>Filtres</legend>
+        <div>
+          <label for="accèsDemande">Accès Directe à la Demande</label>
+          <input type="text" size="35">
+        </div>
+        <!-- Nouveaux champs de texte -->
+        <div>
+          <label for="">Contenant du texte</label>
+          <input type="text" size="35">
+        </div>
 
-<section id="modif">
-  <form class="formmodif" name="formmodif" action="" method="POST">
-    <fieldset id="infos">
-      <legend>Filtres</legend>
-      <div>
-        <label for="accèsDemande">Accès Directe à la Demande</label>
-        <input type="text" size="35">
-      </div>
-      <!-- Nouveaux champs de texte -->
-      <div>
-        <label for="">Contenant du texte</label>
-        <input type="text" size="35">
-      </div>
-
-      <!-- Nouvelles listes déroulantes -->
-      <div>
-        <label for="select_departement">Domaine</label>
-        <select>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-          <!-- Ajoutez d'autres options selon vos besoins -->
-        </select>
-      </div>
-      <div>
-        <label for="select_poste">Demandes du groupe</label>
-        <select name="select_poste" id="select_poste">
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-          <!-- Ajoutez d'autres options selon vos besoins -->
-        </select>
-      </div>
-    </fieldset>
-  </form>
-</section>
-
-
+        <!-- Nouvelles listes déroulantes -->
+        <div>
+          <label for="select_departement">Domaine</label>
+          <select>
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+            <!-- Ajoutez d'autres options selon vos besoins -->
+          </select>
+        </div>
+        <div>
+          <label for="select_poste">Demandes du groupe</label>
+          <select name="select_poste" id="select_poste">
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+            <!-- Ajoutez d'autres options selon vos besoins -->
+          </select>
+        </div>
+      </fieldset>
+    </form>
+  </section>
 </body>
 
 </html>
