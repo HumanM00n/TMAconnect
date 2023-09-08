7395369f350668291342b430177473e39a3f5c84
Mathis ROUY <c1120@cnetp.fr>
2023-09-08 17:11:15 +0200

    Update

--- demande/AffichageDemande.php
+++ demande/AffichageDemande.php
@@ -9,132 +9,120 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
     <title>TMA - Affichage des demandes</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="../css/csshome.css" rel="stylesheet" type="text/css" />
     <link href="../css/AffichageDemande.css" rel="stylesheet" type="text/css" />
     <link rel="icon" href="../img/NLogo2.png" />
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="https://unpkg.com/file-saver/dist/FileSaver.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
+    <?php include('../includes/connexion.inc.php') ?>
     <?php include('../includes/header.html.inc.php') ?>
 
+
     <?php
-    // informations de connexion à la base de données MySQL
-    $servername = "localhost:3308"; // nom du serveur
-    $username = "root"; // nom d'utilisateur
-    $password = "XVsikn92"; // mot de passe
-    $dbname = "tmaconnect"; // nom de la base de données
-// création d'une connexion à la base de données
     try {
-        $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT IdDemande , dom_dmd , libelle , qual_dmd , date_crea , etat_dmd FROM tc_demandes";
+        $stmt = $pdo->query($sql);
 
-        // Votre code ici...
+        if ($stmt === false) {
+            die("Erreur dans la requête SQL");
+        }
+
+        // Vérification des résultats
+        if ($stmt->rowCount() > 0) {
+            $count = $stmt->rowCount();
+
+            // Nombre d'enregistrements par page
+            $nombreParPage = 8;
+
+            // Récupérer le numéro de page à partir des paramètres de requête GET
+            $page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+            // Calculer la position de départ
+            $positionDepart = ($page - 1) * $nombreParPage;
+
+            $sql = "SELECT IdDemande , dom_dmd , libelle , qual_dmd , date_crea , etat_dmd FROM tc_demandes
+                LIMIT :positionDepart, :nombreParPage";
+
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindValue(':positionDepart', $positionDepart, PDO::PARAM_INT);
+            $stmt->bindValue(':nombreParPage', $nombreParPage, PDO::PARAM_INT);
+            $stmt->execute();
+        }
     } catch (PDOException $e) {
-        echo "La connexion a �chou� : " . $e->getMessage();
-    }
-    // include('../');
-    
-    // Requ�te SELECT pour r�cup�rer toutes les demandes
-    $sql = "SELECT D.IdDemande, D.dom_dmd, D.libelle, D.qual_dmd, D.date_crea, E.charge_eval, ET.libelle , M.date_mep
-        FROM tc_demandes D 
-        INNER JOIN tc_ldoi L ON L.IdLdoi = D.IdLdoi
-        INNER JOIN tc_eval E ON E.IdEval = D.IdEval
-        INNER JOIN tc_recette R ON R.IdRecette = D.IdRecette
-        INNER JOIN tc_mep M ON M.IdMep = D.IdMep
-        INNER JOIN tc_etat ET ON ET.IdEtat = D.etat_dmd
-        INNER JOIN tc_domaine DOM ON DOM.IdDomaine = D.dom_dmd
-        ORDER BY IdDemande;";
-
-    $result = $pdo->query($sql);
-
-    // Vérification des résultats
-    if ($stmt->rowCount() > 0) {
-        $count = $stmt->rowCount();
-
-        // Nombre d'enregistrements par page
-        $nombreParPage = 8;
-
-        // Récupérer le numéro de page à partir des paramètres de requête GET
-        $page = isset($_GET['page']) ? $_GET['page'] : 1;
-
-        // Calculer la position de départ
-        $positionDepart = ($page - 1) * $nombreParPage;
-
-        // Modifier la requête SQL avec la clause LIMIT
-        $sql = "SELECT D.IdDemande, D.dom_dmd, D.libelle, D.qual_dmd, D.date_crea, E.charge_eval, ET.libelle , M.date_mep
-    FROM tc_demandes D 
-    INNER JOIN tc_ldoi L ON L.IdLdoi = D.IdLdoi
-    INNER JOIN tc_eval E ON E.IdEval = D.IdEval
-    INNER JOIN tc_recette R ON R.IdRecette = D.IdRecette
-    INNER JOIN tc_mep M ON M.IdMep = D.IdMep
-    INNER JOIN tc_etat ET ON ET.IdEtat = D.etat_dmd
-    INNER JOIN tc_domaine DOM ON DOM.IdDomaine = D.dom_dmd
-    ORDER BY IdDemande
-    LIMIT :positionDepart, :nombreParPage;";
-
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':positionDepart', $positionDepart, PDO::PARAM_INT);
-        $stmt->bindValue(':nombreParPage', $nombreParPage, PDO::PARAM_INT);
-        $stmt->execute();
-
-        // Création du tableau HTML        
-        echo "<table class='table' id='table'>
-        <thead class='table-light'>
-            <tr>
-                <th>🔎</th>
-                <th>N°demande</th>
-                <th>Domaine</th>
-                <th>Libellé</th>
-                <th>Demande crée</th>
-                <th>Charge</th>
-                <th>Etat</th>
-                <th>Date MEP</th>
-                <th class='action'>Télécharger</th>
-            </tr>
-        </thead>
-        <tbody>";
-
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo "<tr>
-            <td>" . $row[":IdDemande"] . "</td>
-            <td>" . $row[":dom_dmd"] . "</td>
-            <td>" . $row[":libelle"] . "</td>
-            <td>" . $row[":qual_dmd"] . "</td>
-            <td>" . $row[":date_crea"] . "</td>
-            <td>" . $row[":charge_eval"] . "</td>
-            <td>" . $row[":libelle"] . "</td>
-            <td>" . $row[":date_mep"] . "</td>
-            <td><!--<i class='bx bxs-download'></i>--></td>
-        </tr>";
-        }
-
-        echo "</tbody></table>";
-
-        // Affichage du nombre de demandes en titre de tableau
-        if ($count <= 1) {
-            // echo "<div id='table'>$count demande enregistré</div>";
-        } else {
-            // echo "<div id='table'>$count demandes enregistrés</div>";
-        }
+        echo "Erreur de connexion à la base de données : " . $e->getMessage();
     }
     ?>
 
+    <?php if ($stmt->rowCount() > 0): ?>
+        <table class='table' id='table'>
+            <thead>
+                <tr>
+                    <th>🔎</th>
+                    <th>N°demande</th>
+                    <th>Domaine</th>
+                    <th>Libellé</th>
+                    <th>Type</th>
+                    <th>Demande créée</th>
+                    <th>Charge</th>
+                    <th>Etat</th>
+                    <th>Date MEP</th>
+                    <th>Télécharger</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+                    <tr>
+                        <td>🔎</td>
+                        <td><?= $row["IdDemande"] ?></td>
+                        <td><?= $row["dom_dmd"] ?></td>
+                        <td><?= $row["libelle"] ?></td>
+                        <td><?= $row["qual_dmd"] ?></td>
+                        <td><?= $row["date_crea"] ?></td>
+                        <td>2</td>
+                        <td><?= $row["etat_dmd"] ?></td>
+                        <td>30-09-2023</td>
+                        <td><i class='bx bxs-download'></i></td>
+                    </tr>
+                <?php endwhile; ?>
+
+            </tbody>
+        </table>
+
+        <!-- Affichage du nombre de demandes en titre de tableau -->
+        <?php
+        // Cr�ation du tableau HTML
+        echo "<table id='idTable' name='idTable'>";
+        // Affichage du nombre d'employ�s en titre de tableau
+        if ($count <= 1) {
+            echo "<div id='tableau'>$count demande enregistré</div>";
+        } else {
+            echo "<div id='tableau'>$count demandes enregistrés</div>";
+        }
+        ?>
+
+    <?php else: ?>
+        <div>Aucune demande trouvée</div>
+    <?php endif; ?>
+
     <script>
         function redirectModifierPage(idUtilisateur) {
-            window.location.href = "../user/Modifier.php?id=" + idUtilisateur;
+            window.location.href = "../user/AffichageDemande.dm?id=" + idUtilisateur;
         }
     </script>
 
+    <!-- PAGINATION -->
 
-    <nav aria-label="...">
+    <!-- <nav aria-label="..." style="display: flex; justify-content: center; align-items: flex-end;"> 
         <ul class="pagination">
             <li class="page-item disabled">
                 <a class="page-link">Previous</a>
@@ -148,7 +136,8 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
                 <a class="page-link" href="#">Next</a>
             </li>
         </ul>
-    </nav>
+    </nav> -->
+
 
     <div id="alertContainer"></div>
 
