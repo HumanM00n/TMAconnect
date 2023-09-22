edec605cc1c0390bc72abe0034e8dd2c83163e75
Mathis ROUY <c1120@cnetp.fr>
2023-09-22 16:46:24 +0200

    Ajout fichier affichage de du detail de la demande

---
+++ demande/detailDemande.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>TMA - Affichage des demandes</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="icon" href="../img/NLogo2.png" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://unpkg.com/file-saver/dist/FileSaver.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link href="../css/AffichageDemande.css" rel="stylesheet">
+    <link href="../css/csshome.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <?php include('../includes/connexion.inc.php') ?>
+    <?php include('../includes/header.html.inc.php') ?>
+    
+</body>
+</html>
