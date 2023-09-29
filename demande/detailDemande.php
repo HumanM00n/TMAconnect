c94f493c8ad1faeb2a4117b50f6e6d87e89287d5
Mathis ROUY <c1120@cnetp.fr>
2023-09-29 18:29:20 +0200

    UPDATE

--- demande/detailDemande.php
+++ demande/detailDemande.php
@@ -20,6 +20,142 @@
 <body>
     <?php include('../includes/connexion.inc.php') ?>
     <?php include('../includes/header.html.inc.php') ?>
+
+    <?php
+
+    // Informations de connexion � la base de donn�es MySQL
+    $servername = "localhost:3308"; // nom du serveur
+    $username = "root"; // nom d'utilisateur
+    $password = "XVsikn92"; // mot de passe
+    $dbname = "tmaconnect"; // nom de la base de donn�es
+    // Cr�ation d'une connexion � la base de donn�es avec PDO
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    // Configuration des attributs de PDO
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Requ�tes SQL pour r�cup�rer les donn�es des tables tc_domaine, tc_qualification et tc_priorite
+    
+    $sql0 = "SELECT IdDomaine, libelle FROM tc_domaine";
+    // $sql0 = "SELECT libelle, IdDomaine FROM tc_domaine";
+    $stmt0 = $pdo->query($sql0);
+    $result0 = $stmt0->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql1 = "SELECT IdQual ,libelle FROM tc_qualif";
+    $stmt1 = $pdo->query($sql1);
+    $result1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql2 = "SELECT IdPriorite, libelle FROM tc_priorite";
+    $stmt2 = $pdo->query($sql2);
+    $result2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+
+    // Requ�tes SQL pour r�cup�rer les donn�es de la table tc_utilisateur
+    $sql3 = "SELECT IdUtil, nom FROM tc_utilisateur";
+    $stmt3 = $pdo->query($sql3);
+    $result3 = $stmt3->fetchAll(PDO::FETCH_ASSOC);
+
+    // Requ�tes SQL pour r�cup�rer les donn�es de la table tc_utilisateur avec une clause where pour le "Signataire"
+    $sql4 = "SELECT IdUtil, nom FROM tc_utilisateur WHERE S_users = '1' OR S_users = '4'";
+    $stmt4 = $pdo->query($sql4);
+    $result4 = $stmt4->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql5 = "SELECT IdRegroupe, libelle FROM tc_regroupement";
+    $stmt5 = $pdo->query($sql5);
+    $result5 = $stmt5->fetchAll(PDO::FETCH_ASSOC);
+
+    // Requ�tes SQL pour r�cup�rer les donn�es de la table tc_etat
+    $sql6 = "SELECT IdEtat, libelle FROM tc_etat";
+    $stmt6 = $pdo->query($sql6);
+    $result6 = $stmt6->fetchAll(PDO::FETCH_ASSOC);
+
+    // Requ�tes SQL pour r�cup�rer les donn�es de la table tc_Benef
+    $sql7 = "SELECT IdBenef, lbl_benef FROM tc_benef";
+    $stmt7 = $pdo->query($sql7);
+    $result7 = $stmt7->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+    <?php
+    if (
+        isset($_POST["selectDom"])
+        && isset($_POST["selectQualif"])
+        && isset($_POST["selectPrio"])
+        && isset($_POST["demLibelle"]) //N°Demande ENTRE selectDemandePar et demCree
+        && isset($_POST["demCree"])
+        && isset($_POST["selectDemandePar"])
+        && isset($_POST["demEmise"])
+        && isset($_POST["selectDemandeEmisePar"])
+        && isset($_POST["demRecu"])
+        && isset($_POST["selectBeneficiaire"])
+        && isset($_POST["demEtat"])
+        && isset($_POST["selectDemandeEtat"])
+        && isset($_POST["visaServEtude"])
+        && isset($_POST["selectSignataire"])
+        && isset($_POST["selectAffectation"])
+        && isset($_POST["demFs"])
+        && isset($_POST["demAmortis"])
+        && isset($_POST["demRct"])
+        && isset($_POST["selectRegroupement"])
+        && isset($_POST["demArchiv"])
+    ) {
+        $var1 = $_POST["selectDom"];
+        $var2 = $_POST["selectQualif"];
+        $var3 = $_POST["selectPrio"];
+        $var4 = $_POST["demLibelle"];
+        $var5 = $_POST["demCree"];
+        $var6 = $_POST["selectDemandePar"];
+        $var7 = $_POST["demEmise"];
+        $var8 = $_POST["selectDemandeEmisePar"];
+        $var9 = $_POST["demRecu"];
+        $var10 = $_POST["selectBeneficiaire"];
+        $var11 = $_POST["demEtat"];
+        $var12 = $_POST["selectDemandeEtat"];
+        $var13 = $_POST["visaServEtude"];
+        $var14 = $_POST["selectSignataire"];
+        $var15 = $_POST["selectAffectation"];
+        $var16 = $_POST["demFs"];
+        $var17 = $_POST["demAmortis"];
+        $var18 = $_POST["demRct"];
+        $var19 = $_POST["selectRegroupement"];
+        $var20 = $_POST["demArchiv"];
+
+        // Préparation de la requête d'insertion en utilisant des paramètres nommés
+        $sql = "INSERT INTO tc_demandes (dom_dmd, qual_dmd, prt_dmd, libelle, date_crea, util_crea, date_emet, util_emet, date_recu, util_benef, date_etat_dmd, etat_dmd, date_visa_dmd, util_sign_dmd, util_affect_dmd, date_fs, amorti_dmd, date_rct_prvu, regroupement, date_archiv, IdLdoi, IdEval, IdRecette, IdMep) 
+                VALUES (:var1, :var2, :var3, :var4, :var5, :var6, :var7, :var8, :var9, :var10, :var11, :var12, :var13, :var14, :var15, :var16, :var17, :var18, :var19, :var20, 1, 1, 1, 1)";
+
+        $stmt = $pdo->prepare($sql);
+
+        // Binder les valeurs aux paramètres nommés
+        $stmt->bindValue(":var1", $var1);
+        $stmt->bindValue(":var2", $var2);
+        $stmt->bindValue(":var3", $var3);
+        $stmt->bindValue(":var4", $var4);
+        $stmt->bindValue(":var5", $var5);
+        $stmt->bindValue(":var6", $var6);
+        $stmt->bindValue(":var7", $var7);
+        $stmt->bindValue(":var8", $var8);
+        $stmt->bindValue(":var9", $var9);
+        $stmt->bindValue(":var10", $var10);
+        $stmt->bindValue(":var11", $var11);
+        $stmt->bindValue(":var12", $var12);
+        $stmt->bindValue(":var13", $var13);
+        $stmt->bindValue(":var14", $var14);
+        $stmt->bindValue(":var15", $var15);
+        $stmt->bindValue(":var16", $var16);
+        $stmt->bindValue(":var17", $var17);
+        $stmt->bindValue(":var18", $var18);
+        $stmt->bindValue(":var19", $var19);
+        $stmt->bindValue(":var20", $var20);
+
+        try {
+            if ($stmt->execute()) {
+                echo '<center><p class="Successful">Données insérées avec succès!</p></center>';
+            } else {
+                echo "Erreur lors de l'insertion des données.";
+            }
+        } catch (PDOException $e) {
+            echo "Erreur de connexion à la base de données : " . $e->getMessage();
+        }
+    }
+    ?>
     
 </body>
 </html>
