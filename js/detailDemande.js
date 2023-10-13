727a42323200baab166c26169700dbe50a38f203
Mathis ROUY <c1120@cnetp.fr>
2023-10-13 15:45:19 +0200

    Fonctionnement du déroulement du détail de la demande

--- js/detailDemande.js
+++ js/detailDemande.js
@@ -1,29 +1,31 @@
-// let afficherDetails = document.getElementById('menuNouvelDmd');
-
 document.querySelectorAll('#monBouton').forEach(bouton => {
     bouton.addEventListener('click', function () {
-        fetch('test-code2.php')
+        let id_demande = this.dataset.id
+        console.log(id_demande);
+        let url =`test-code2.php?id_demande=${encodeURIComponent(id_demande)}`;
+
+        fetch(url)
             .then(response => response.text())
             .then(data => {
-                console.log(data);
+                const row = this.parentElement.parentElement; // Récupère la ligne parente du bouton
+                const detailsDemande = document.createElement('td'); // Crée une nouvelle ligne pour les détails
+
+                detailsDemande.innerHTML = data;
+
+                if (!row.classList.contains('details-visible')) {
+                    // row.classList.add('details-visible');
+                    row.after(detailsDemande); // Ajoute les détails après la ligne de demande
+                } else {
+                    // row.classList.remove('details-visible');
+                    const detailsRow = row.nextElementSibling; // Récupère la ligne des détails
+                    if (detailsRow && detailsRow.classList.contains('afficherDetails')) {
+                        detailsRow.remove(); // Supprime les détails s'ils sont déjà visibles
+                    }
+                }
             })
             .catch((error) => {
                 console.error('Erreur :', error);
             });
-        const row = this.parentElement.parentElement; // Récupère la ligne parente du bouton
-        const detailsDemande = document.createElement('tr'); // Crée une nouvelle ligne pour les détails
 
-        detailsDemande.innerHTML = '<td colspan="10">bonjour</td>';
-
-        if (!row.classList.contains('details-visible')) {
-            row.classList.add('details-visible');
-            row.after(detailsDemande); // Ajoute les détails après la ligne de demande
-        } else {
-            row.classList.remove('details-visible');
-            const detailsRow = row.nextElementSibling; // Récupère la ligne des détails
-            if (detailsRow && detailsRow.classList.contains('afficherDetails')) {
-                detailsRow.remove(); // Supprime les détails s'ils sont déjà visibles
-            }
-        }
     });
 });
