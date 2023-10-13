43a681cef5ce6886681f58d4053c588d49ee241b
HumanM00n <113510790+HumanM00n@users.noreply.github.com>
2023-10-13 15:54:42 +0200

    Retirage des lignes commentées

--- js/date.js
+++ js/date.js
@@ -10,17 +10,3 @@ $(function() {
   });
   $("#datepicker").attr('type', 'date'); // Convertit le champ en texte pour désactiver le datepicker natif
 });
-
-
-
-
-
-
-// $( function() {
-//   $("#datepicker").datepicker({
-//       dateFormat: 'dd/mm/yy',
-//       monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
-//       monthNamesShort: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Août', 'Sep', 'Oct', 'Nov', 'Déc'],
-//       dayNamesMin: ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim']
-//   });
-// });
