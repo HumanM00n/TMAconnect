0f41d6a928a26ffa3b25354620e557c8e3117b2e
HumanM00n <113510790+HumanM00n@users.noreply.github.com>
2023-10-20 11:47:55 +0200

    Application du bouton de redirection pour afficher le détail de la demande

--- demande/AffichageDemande.php
+++ demande/AffichageDemande.php
@@ -6,40 +6,40 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
 <html>
 
 <head>
-    <title>TMA - Affichage Demandes</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href="../css/csshome.css" rel="stylesheet" type="text/css" />
-    <link rel="icon" href="../img/NLogo2.png" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <script src="https://unpkg.com/file-saver/dist/FileSaver.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <link href="../css/AffichageDemande.css" rel="stylesheet">
+  <title>TMA - Affichage Demandes</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="icon" href="../img/NLogo2.png" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script src="https://unpkg.com/file-saver/dist/FileSaver.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link href="../css/csshome.css" rel="stylesheet" type="text/css" />
+  <link href="../css/AffichageDemande.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-    <?php include('../includes/connexion.inc.php') ?>
-    <?php include('../includes/header.html.inc.php') ?>
+  <?php include('../includes/connexion.inc.php') ?>
+  <?php include('../includes/header.html.inc.php') ?>
 
-    <?php
-    // Requ�tes SQL pour le filtre de recherche des demandes 
-    
-    $sql0 = "SELECT libelle FROM tc_domaine";
-    $stmt0 = $pdo->query($sql0);
-    $result0 = $stmt0->fetchAll(PDO::FETCH_ASSOC);
+  <?php
+  // Requ�tes SQL pour le filtre de recherche des demandes 
+  
+  $sql0 = "SELECT libelle FROM tc_domaine";
+  $stmt0 = $pdo->query($sql0);
+  $result0 = $stmt0->fetchAll(PDO::FETCH_ASSOC);
 
-    // Requ�tes SQL pour r�cup�rer les donn�es de la table tc_etat
-    $sql1 = "SELECT libelle FROM tc_etat";
-    $stmt1 = $pdo->query($sql1);
-    $result1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
-    ?>
+  // Requ�tes SQL pour r�cup�rer les donn�es de la table tc_etat
+  $sql1 = "SELECT libelle FROM tc_etat";
+  $stmt1 = $pdo->query($sql1);
+  $result1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+  ?>
 
-    <section class="modif">
+  <section class="modif">
     <form class="formmodif" name="formmodif" action="" method="POST">
       <fieldset id="infos">
         <legend>Filtres</legend>
@@ -70,7 +70,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
             ?>
           </select>
         </div>
-        
+
         <div class="label-container">
           <label for="num-dmd">N°Demande</label>
           <input type="number" id="num-dmd" name="num-dmd" size="35">
@@ -79,7 +79,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
         <div>
           <!-- Nouveaux champs de texte -->
           <label for="input_lib">Libellé de la Demande</label>
-          <input type="text" name="input-lib" id="input_lib"> 
+          <input type="text" name="input-lib" id="input_lib">
         </div>
 
 
@@ -91,117 +91,131 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
     </form>
   </section>
 
-
-    <?php
-    try {
-        $sql = "SELECT D.IdDemande , DOM.libelle , D.libelle , Q.libelle , D.date_crea , E.libelle 
+<!----------------------------------------
+|  Requêtes pour récupérer les demandes  | 
+------------------------------------------>
+  <?php
+  try {
+    $sql = "SELECT D.IdDemande , DOM.libelle , D.libelle , Q.libelle , D.date_crea , E.libelle 
                 FROM tc_demandes D, tc_domaine DOM , tc_qualif Q , tc_etat E
                 WHERE D.dom_dmd = DOM.IdDomaine 
                 AND D.qual_dmd = Q.IdQual
                 AND D.etat_dmd = E.IdEtat";
 
-        $stmt = $pdo->query($sql);
+    $stmt = $pdo->query($sql);
 
-        if ($stmt === false) {
-            die("Erreur dans la requête SQL");
-        }
-
-        // Vérification des résultats
-        if ($stmt->rowCount() > 0) {
-            $count = $stmt->rowCount();
-
-            // Nombre d'enregistrements par page
-            $nombreParPage = 8;
-
-            // Récupérer le numéro de page à partir des paramètres de requête GET
-            $page = isset($_GET['page']) ? $_GET['page'] : 1;
-
-            // Calculer la position de départ
-            $positionDepart = ($page - 1) * $nombreParPage;
-
-            // $sql = "SELECT IdDemande , dom_dmd , libelle , qual_dmd , date_crea , etat_dmd FROM tc_demandes
-            //     LIMIT :positionDepart, :nombreParPage";
-
-            $stmt = $pdo->prepare($sql);
-            // $stmt->bindValue(':positionDepart', $positionDepart, PDO::PARAM_INT);
-            // $stmt->bindValue(':nombreParPage', $nombreParPage, PDO::PARAM_INT);
-            $stmt->execute();
-        }
-    } catch (PDOException $e) {
-        echo "Erreur de connexion à la base de données : " . $e->getMessage();
+    if ($stmt === false) {
+      die("Erreur dans la requête SQL");
     }
-    ?>
 
-    <?php if ($stmt->rowCount() > 0): ?>
-        <table class='table' id='table'>
-            <thead>
-                <tr>
-                    <th>🔎</th>
-                    <th>N°demande</th>
-                    <th>Domaine</th>
-                    <th>Libellé</th>
-                    <th>Type</th>
-                    <th>Demande créée</th>
-                    <th>Charge</th>
-                    <th>Etat</th>
-                    <th>Date MEP</th>
-                    <th>Télécharger</th>
-                </tr>
-            </thead>
-            <tbody>
+    // Vérification des résultats
+    if ($stmt->rowCount() > 0) {
+      $count = $stmt->rowCount();
 
-                <?php while ($row = $stmt->fetch(PDO::FETCH_BOTH)): ?>
-                    <tr>
-                        <!-- <td><a href="./detailDemande.php?idDemande=<?php echo $row[0]?>">🔎</a></td> -->
-                        <td>🔎</td>
-                        <td><?= $row[0] ?></td>
-                        <td><?= $row[1] ?></td> 
-                        <td><?= $row[2] ?></td> 
-                        <td><?= $row[3] ?></td> 
-                        <td><?= $row[4] ?></td> 
-                        <td>2</td>
-                        <td><?= $row[5] ?></td> 
-                        <td>30-09-2023</td>
-                        <td><i class='bx bx-download'></i></td>
-                    </tr>
-                <?php endwhile; ?>
+      // Nombre d'enregistrements par page
+      $nombreParPage = 8;
 
-            </tbody>
-        </table>
+      // Récupérer le numéro de page à partir des paramètres de requête GET
+      $page = isset($_GET['page']) ? $_GET['page'] : 1;
 
-    <?php else: ?>
-        <div>Aucune demande trouvée</div>
-    <?php endif; ?>
+      // Calculer la position de départ
+      $positionDepart = ($page - 1) * $nombreParPage;
 
-    <script>
-        function redirectModifierPage(idUtilisateur) {
-            window.location.href = "../user/AffichageDemande.dm?id=" + idUtilisateur;
-        }
-    </script>
+      // $sql = "SELECT IdDemande , dom_dmd , libelle , qual_dmd , date_crea , etat_dmd FROM tc_demandes
+      //     LIMIT :positionDepart, :nombreParPage";
+  
+      $stmt = $pdo->prepare($sql);
+      // $stmt->bindValue(':positionDepart', $positionDepart, PDO::PARAM_INT);
+      // $stmt->bindValue(':nombreParPage', $nombreParPage, PDO::PARAM_INT);
+      $stmt->execute();
+    }
+  } catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données : " . $e->getMessage();
+  }
+  ?>
 
-   <!-- PAGINATION TABLEAU DEMANDE -->
+<!-----------------------------------------
+|           Tableau Des demandes          | 
+------------------------------------------>
 
-    <nav class="div--pagination" aria-label="..."> 
-        <ul class="pagination">
-            <li class="page-item disabled">
-                <a class="page-link">Previous</a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item active" aria-current="page">
-                <a class="page-link" href="#">2</a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-                <a class="page-link" href="#">Next</a>
-            </li>
-        </ul>
-    </nav>
+  <?php if ($stmt->rowCount() > 0): ?>
+    <table class='table' id='table'>
+      <thead>
+        <tr>
+          <th>🔎</th>
+          <th>N°demande</th>
+          <th>Domaine</th>
+          <th>Libellé</th>
+          <th>Type</th>
+          <th>Demande créée</th>
+          <th>Charge</th>
+          <th>Etat</th>
+          <th>Date MEP</th>
+          <th>Télécharger</th>
+        </tr>
+      </thead>
+      <tbody>
 
-    
-    <div id="alertContainer"></div>
+        <?php while ($row = $stmt->fetch(PDO::FETCH_BOTH)): ?>
+          <tr class="afficherDetails">
+            <td><button><a href="detailDemande.php?idDemande=<?= $row[0] ?>">🔎</a></button></td>
+            <td>
+              <?= $row[0] ?>
+            </td>
+            <td>
+              <?= $row[1] ?>
+            </td>
+            <td>
+              <?= $row[2] ?>
+            </td>
+            <td>
+              <?= $row[3] ?>
+            </td>
+            <td>
+              <?= $row[4] ?>
+            </td>
+            <td>2</td>
+            <td>
+              <?= $row[5] ?>
+            </td>
+            <td>30-09-2023</td>
+            <td><i class='bx bx-download'></i></td>
+          </tr>
 
-    <!-- <script src="../js/Filtre-dmd.js"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+        <?php endwhile; ?>
+
+      </tbody>
+    </table>
+
+  <?php else: ?>
+    <div>Aucune demande trouvée</div>
+  <?php endif; ?>
+
+<!--------------------------------------------
+|             Pagination Du Tableau          |  
+--------------------------------------------->
+
+  <nav class="div--pagination" aria-label="...">
+    <ul class="pagination">
+      <li class="page-item disabled">
+        <a class="page-link">Previous</a>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item active" aria-current="page">
+        <a class="page-link" href="#">2</a>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#">Next</a>
+      </li>
+    </ul>
+  </nav>
+
+
+  <div id="alertContainer"></div>
+
+  <!-- <script src="../js/Filtre-dmd.js"></script> -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
