e59ffd132375f51c9475d936139ff1e2acf3e0fd
Mathis Rouy <c1120@cnetp.fr>
2023-11-03 19:18:10 +0100

    Suppression du doublon du tableau des demandes

--- demande/AffichageDemande.php
+++ demande/AffichageDemande.php
@@ -43,11 +43,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
         <!-- Liste déroulante pour le domaine -->
         <div class="label-container">
           <label for="select_domaine2">Domaine</label>
-          <select name="select_domaine" id="select_domaine">
-
-          <!-- <option value='1' >1</option>
-          <option value='2' >2</option> -->
-
+          <select name="select_domaine" id="select_domaine" data-filter>
             <?php
             echo "<option value=''></option>";
             foreach ($result0 as $row) {
@@ -61,16 +57,15 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
 
         <div class="label-container">
           <label for="select_etat2">Etat</label>
-          <select name="select_etat" id="select_etat">
+          <select name="select_etat" id="select_etat" data-filter>
             <?php
-            echo "<option value='' disabled selected hidden></option>";
+            echo "<option value=''></option>";
             foreach ($result1 as $row) {
               $id_etat = $row['IdEtat']; 
               $lib_etat = $row['libelle'];
               echo "<option value='$id_etat'>$lib_etat</option>";
             }
             ?>
-          </select>
           </select>
         </div>
 
@@ -84,7 +79,8 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
           <label for="input_lib">Libellé de la Demande</label>
           <!-- <input type="text" name="input-lib" id="input_lib"> -->
         </div>
-
+      
+        
 
         <div class="bloc-btn">
           <button type="submit">Appliquer</button>
@@ -93,43 +89,6 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
       </fieldset>
     </form>
   </section>
-
-<!----------------------------------------
-|  Requêtes pour récupérer les demandes  | 
------------------------------------------->
-  <?php
-  try {
-    $sql = "SELECT D.IdDemande , DOM.libelle , D.libelle , Q.libelle , D.date_crea , E.libelle 
-                FROM tc_demandes D, tc_domaine DOM , tc_qualif Q , tc_etat E
-                WHERE D.dom_dmd = DOM.IdDomaine 
-                AND D.qual_dmd = Q.IdQual
-                AND D.etat_dmd = E.IdEtat";
-
-    $stmt = $pdo->query($sql);
-
-    if ($stmt === false) {
-      die("Erreur dans la requête SQL");
-    }
-
-    // Vérification des résultats
-    if ($stmt->rowCount() > 0) {
-      $count = $stmt->rowCount();
-
-      // Nombre d'enregistrements par page
-      $nombreParPage = 8;
-
-      // Récupérer le numéro de page à partir des paramètres de requête GET
-      $page = isset($_GET['page']) ? $_GET['page'] : 1;
-
-      // Calculer la position de départ
-      $positionDepart = ($page - 1) * $nombreParPage;
-      $stmt = $pdo->prepare($sql);
-      $stmt->execute();
-    }
-  } catch (PDOException $e) {
-    echo "Erreur de connexion à la base de données : " . $e->getMessage();
-  }
-  ?>
 
 <!-----------------------------------------
 |           Tableau Des demandes          | 
@@ -152,22 +111,22 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
   <nav class="div--pagination" aria-label="...">
     <ul class="pagination">
       <li class="page-item disabled">
-        <a class="page-link">Previous</a>
+        <a class="page-link">Précédent</a>
       </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item active" aria-current="page">
+      <li class="page-item active"><a class="page-link" href="#">1</a></li>
+      <li class="page-item" aria-current="page">
         <a class="page-link" href="#">2</a>
       </li>
       <li class="page-item"><a class="page-link" href="#">3</a></li>
       <li class="page-item">
-        <a class="page-link" href="#">Next</a>
+        <a class="page-link" href="#">Suivant</a>
       </li>
     </ul>
   </nav>
 
   <div id="alertContainer"></div>
 
-  <script src="../js/AJAX.js"></script>
+  <!-- <script src="../js/AJAX.js"></script> -->
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
