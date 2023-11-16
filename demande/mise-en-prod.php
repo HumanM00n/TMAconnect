01847fc6fe74e9697b273e392432ab38cce8431e
Mathis Rouy <c1120@cnetp.fr>
2023-11-16 14:47:43 +0100

    Nettoyage des liens

--- demande/mise-en-prod.php
+++ demande/mise-en-prod.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/csshome.css">
     <link rel="stylesheet" href="../css/mise-en-prod.css">
     <link rel="icon" href="../img/NLogo2.png" />
-    <title>TMAconnect-ldoi</title>
+    <title>TMAconnect-mep</title>
+    
 </head>
 <?php include('../includes/connexion.inc.php') ?>
 <?php include('../includes/header.html.inc.php') ?>
@@ -52,7 +53,9 @@
             </div> -->
         </form>
     </section>
+   
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
