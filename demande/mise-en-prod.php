f836a55f061c4df401076e18278043bb2c77049c
Mathis Rouy <c1120@cnetp.fr>
2023-11-17 16:40:52 +0100

    Changement du titre de la page

--- demande/mise-en-prod.php
+++ demande/mise-en-prod.php
@@ -5,12 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/csshome.css">
     <link rel="stylesheet" href="../css/mise-en-prod.css">
     <link rel="icon" href="../img/NLogo2.png" />
-    <title>TMAconnect-mep</title>
+    <title>TMAmep</title>
     
 </head>
 <?php include('../includes/connexion.inc.php') ?>
