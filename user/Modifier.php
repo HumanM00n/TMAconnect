5a1212efaec7fbec259028757d06c4c552666d2e
Mathis Rouy <c1120@cnetp.fr>
2023-11-17 15:17:03 +0100

    UPDATE

--- user/Modifier.php
+++ user/Modifier.php
@@ -9,10 +9,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
     <link href="../css/csshome.css" rel="stylesheet" type="text/css" />
     <link rel="icon" href="../img/NLogo2.png" />
 
@@ -244,8 +241,8 @@ WHERE U.IdUtil = :idUtilisateur";
                                 </div>
                             </div>
 
-                            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-                            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+                            <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
                             <script>
                                 $(document).ready(function () {
                                     $('.toast').toast('show');
@@ -266,8 +263,8 @@ WHERE U.IdUtil = :idUtilisateur";
                                 </div>
                             </div>
 
-                            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-                            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+                            <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
                             <script>
                                 $(document).ready(function () {
                                     $('.toast').toast('show');
@@ -284,7 +281,9 @@ WHERE U.IdUtil = :idUtilisateur";
             </fieldset>
         </form>
     </section>
-
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
+
