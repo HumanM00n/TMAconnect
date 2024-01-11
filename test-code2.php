066ac477b13fc4913cfe924b150d938fe3db9e47
Mathis Rouy <c1120@cnetp.fr>
2024-01-11 16:21:54 +0100

    Début du développement de la recette

--- test-code2.php
+++ test-code2.php
@@ -1,13 +1,67 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>TC2</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="css/test-css.css" rel="stylesheet" type="text/css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="css/csshome.css"> -->
+    <link rel="stylesheet" href="css/test-css.css">
     <link rel="icon" href="img/NLogo2.png" />
+    <title>TC2 - Eval</title>
 </head>
+<?php include('includes/connexion.inc.php') ?>
+
+<body>
+    <!------------------------------------------
+  |             BOUTON "RETOUR"              | 
+  ------------------------------------------->
+    <div class="btnretour">
+        <button onClick=" history.back();">Retour</button>
+    </div>
+    <main>
+        <section class="container" id="container">
+            <h3><b>Evaluation</b></h3>
+            <form class="grid-container" method="post">
+                <div class="row">
+                    <div class="inputGroup col-md-8" id="divDate">
+                        <div class="col-md-4">
+                            <label for="inputDate" class="form-label">Date d'évaluation</label>
+                            <input type="text" class="form-control" id="datepicker" name="datepicker"
+                            pattern="\d{1,2}/\d{1,2}/\d{4}" placeholder="">
+                        </div>
+
+                        <div class="col-md-4" id="divPar">
+                            <label for="inputUtil" class="form-label">Par</label>
+                            <select id="inputUtil" name="inputUtil" class="form-select">
+                                <option value=''>KevinOcarré</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-md-5">
+                        <label for="validationCustom04" class="form-label">Carge</label>
+                        <select id="inputDate" name="inputUtil" class="form-select">
+                            <option value='Tarif'></option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-5">
+                        <label for="validationCustom04" class="form-label">Tarif</label>
+                        <input type="date" class="form-control" id="validationDefault01" required>
+                    </div>
+
+
+                    <div class="col-md-6">
+                        <label for="validationCustom04" class="form-label">Tarif</label>
+                        <input type="date" class="form-control" id="validationDefault01" required>
+                    </div>
+                </div>
+            </form>
+        </section>
+    </main>
+
+</body>
+
+</html>
