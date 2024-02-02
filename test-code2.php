b04af1f11441ec5c3f5345e1cf088c8f7cd713ff
Mathis Rouy <c1120@cnetp.fr>
2024-02-02 10:23:55 +0100

    Début du développement de la page Evalution

--- test-code2.php
+++ test-code2.php
@@ -71,9 +71,7 @@
             </form>
         </section>
     </main>
-
-
-
+    
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
         crossorigin="anonymous"></script>
