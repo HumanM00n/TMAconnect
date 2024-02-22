fb965ea5a24ca430c6f6cb954e89dde50cf2aaf2
Mathis Rouy <c1120@cnetp.fr>
2024-02-22 15:57:19 +0100

    Développement de la V2 de index.php

--- test-code3.php
+++ test-code3.php
@@ -7,8 +7,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
   <link href="css/csshome.css" rel="stylesheet" type="text/css" />
   <link href="css/test3.css" rel="stylesheet" type="text/css">
   <link rel="icon" href="img/NLogo2.png" />
@@ -16,134 +16,49 @@
 
 <body>
   <?php include('includes/connexion.inc.php') ?>
-  <?php include('includes/header.html.inc.php') ?>
 
   <?php
 
-  // Requ�tes SQL pour le filtre de recherche des demandes 
-  $sql0 = "SELECT IdDomaine , libelle FROM tc_domaine";
-  $stmt0 = $pdo->query($sql0);
-  $result0 = $stmt0->fetchAll(PDO::FETCH_ASSOC);
-
-  // Requ�tes SQL pour r�cup�rer les donn�es de la table tc_etat
-  $sql1 = "SELECT IdEtat , libelle FROM tc_etat";
-  $stmt1 = $pdo->query($sql1);
-  $result1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
-
+  // // Requ�tes SQL pour le filtre de recherche des demandes 
+  // $sql0 = "SELECT IdDomaine , libelle FROM tc_domaine";
+  // $stmt0 = $pdo->query($sql0);
+  // $result0 = $stmt0->fetchAll(PDO::FETCH_ASSOC);
+  
+  // // Requ�tes SQL pour r�cup�rer les donn�es de la table tc_etat
+  // $sql1 = "SELECT IdEtat , libelle FROM tc_etat";
+  // $stmt1 = $pdo->query($sql1);
+  // $result1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+  
   ?>
+  <main>
 
-  <!------------------------------------------
-  |             BOUTON "RETOUR"              | 
-  ------------------------------------------->
+    <section id="modif">
+      <img src="img/NLogo2.png" alt="">
+      <form class="formmodif" name="formmodif" action="" method="POST">
+        <fieldset class="infos">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
 
-  <a href="demande/creationDemande.php" class="btnajout">
-    <button onClick="window.location.href='demande/creationDemande.php';"> Créer une demande</button>
-  </a>
+        </fieldset>
+      </form>
+    </section>
 
-  <!------------------------------------------
-  |      FILTRE ET TABLEAU DES DEMANDES      | 
-  ------------------------------------------->
+  </main>
 
-  <section class="modif">
-    <form id="formFiltre" class="formmodif" name="formmodif" action="" method="POST">
-      <fieldset id="infos">
-        <legend>Filtres</legend>
-
-        <!-- Liste déroulante pour le domaine -->
-        <div class="label-container">
-          <label for="select_domaine">Domaine</label>
-          <select name="select_domaine" id="select_domaine" data-filter>
-            <?php
-            echo "<option value=''></option>";
-            foreach ($result0 as $row) {
-              $id_dom = $row['IdDomaine'];
-              $lib_dom = $row['libelle'];
-              echo "<option value='$lib_dom'>$lib_dom</option>";
-            }
-            ?>
-          </select>
-        </div>
-
-        <div class="label-container">
-          <label for="select_etat">Etat</label>
-          <select name="select_etat" id="select_etat" data-filter>
-            <?php
-            echo "<option value=''></option>";
-            foreach ($result1 as $row) {
-              $id_etat = $row['IdEtat'];
-              $lib_etat = $row['libelle'];
-              echo "<option value='$lib_etat'>$lib_etat</option>";
-            }
-            ?>
-          </select>
-        </div>
-
-        <div class="label-container">
-          <label for="num_dmd">N°Demande</label>
-          <input type="number" id="num_dmd" name="num_dmd" size="35">
-        </div>
-
-        <div class="label-container" id="divLibelle">
-          <label for="lib_dmd">Libellé de la Demande</label>
-          <input type="text" name="lib_dmd" id="lib_dmd">
-        </div>
-
-
-        <div class="bloc-btn">
-          <button type="submit" id="submit" name="appliquer">Appliquer</button>
-          <button type="reset">Réinitialiser</button>
-        </div>
-      </fieldset>
-    </form>
-  </section>
-
-  <!-----------------------------------------
-|           Tableau Des demandes          | 
------------------------------------------->
-
-  <?php if ($stmt->rowCount() > 0): ?>
-
-    <div id="table-container" class='table'>
-      <?php include_once('test-code4.php'); ?>
-    </div>
-
-  <?php else: ?>
-    <div>Aucune demande trouvée</div>
-  <?php endif; ?>
-
-  <!--------------------------------------------
-|             Pagination Du Tableau          |  
---------------------------------------------->
-
-  <nav class="div--pagination" aria-label="Page navigation example">
-    <ul class="pagination">
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
-          <span aria-hidden="true">&laquo;</span>
-        </a>
-      </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
-          <span aria-hidden="true">&raquo;</span>
-        </a>
-      </li>
-    </ul>
-  </nav>
-
-  <!-- Lien vers jQuery -->
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-
-  <!-- Lien vers Bootstrap.js -->
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Lien vers les script JS -->
-  <script src="js/AJAX.js"></script>
-
+  <!-- LINK JS BOOTSTRAP -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
