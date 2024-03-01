69dafc93226f4c82f940397fe91c6580549b2050
Mathis Rouy <c1120@cnetp.fr>
2024-03-01 14:20:41 +0100

    Ajout du fichier pour appeler un include contenant tous les liens des différentes utilisées

---
+++ includes/script.html.inc.php
@@ -0,0 +1,58 @@
+<!-----------------------------------
+  |         INCLUDES ALL CDN        | 
+  ---------------------------------->
+
+<!-----------------------------
+  |         BOOTSTRAP CDN      | 
+  ---------------------------->
+
+<!--************************ LINK BOOTSTRAP CSS ************************-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+<!-- CDN CLOUDFLARE BOOTSRAP DATEPICKER -->
+<link rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+
+<!-- CDN POPPERS BOOTSTRAP -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css">
+
+
+<!--************************ SCRIPT JS BOOTSTRAP CSS ************************-->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+  integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+  integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+
+
+
+
+<!-----------------------------
+  |         JQUERY CDN        | 
+  ---------------------------->
+
+<!--************************ LINK CDN JQUERY CSS ************************-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/jquery-ui.min.css"
+  integrity="sha512-ELV+xyi8IhEApPS/pSj66+Jiw+sOT1Mqkzlh8ExXihe4zfqbWkxPRi8wptXIO9g73FSlhmquFlUOuMSoXz5IRw=="
+  crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<!--  -->
+<!--************************ SCRIPT CDN JQUERY JS ************************-->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+
+
+<!-------------------------------------
+  |        CDN CLOUDFLARE FONTAWESOME | 
+  ------------------------------------>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<!------------------------------
+  |        BOXICON CDN         | 
+  ----------------------------->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
