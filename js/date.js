8581e34b395a49bb4c612cbb41af704a9282ba2f
Mathis Rouy <c1120@cnetp.fr>
2024-03-01 17:28:46 +0100

    Ajout et Fonctionnement du nouveau script du formatage de la date

--- js/date.js
+++ js/date.js
@@ -1,16 +1,11 @@
 $(function () {
-  $("#datepicker").datepicker({
-    dateFormat: 'dd/mm/yy',
-    monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
-    monthNamesShort: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Août', 'Sep', 'Oct', 'Nov', 'Déc'],
-    dayNamesMin: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
-    minDate: 1, // Aujourd'hui
-    maxDate: '+1y', // Un an à partir d'aujourd'hui
-    beforeShowDay: function (date) {
-      var day = date.getDay();
-      return [(day != 0 && day != 6), ''];
-    },
-    firstDay: 1, // Lundi
-    showButtonPanel: true,
+  (function($){$.fn.datepicker.dates['fr'] = {days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"],daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam."],daysMin: ["di", "lu", "ma", "me", "je", "ve", "sa"],months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],monthsShort: ["janv.", "févr.", "mars", "avril", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."],today: "Aujourd'hui",monthsTitle: "Mois",clear: "Effacer",weekStart: 1,format: "dd/mm/yyyy"};}(jQuery));
+  $('#datepicker').datepicker({
+      isRTL: true,
+      autoclose: true,
+      todayHighlight: true,
+      language: 'fr',
+      format: 'dd/mm/yyyy',
   });
 });
+
