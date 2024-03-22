d15899c5bac6ee132831ff02db6929d2f24773d2
Mathis Rouy <c1120@cnetp.fr>
2024-03-22 17:28:07 +0100

    Finalisation des test de la mise en page de  Evaluation.php

--- test-code2.php
+++ test-code2.php
@@ -4,14 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LINK BOOTSTRAP CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- LINK BOOTSTRAP DATEPICKER CSS -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.css">
+
+    <!-- Autres liens CSS -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="css/csshome.css">
     <link rel="stylesheet" href="css/test2.css">
     <link rel="icon" href="img/NLogo2.png" />
     <title>TC2 - Eval</title>
 </head>
+
 
 <?php include ('includes/connexion.inc.php') ?>
 <?php include ('includes/header.html.inc.php') ?>
@@ -30,13 +38,13 @@
                     <legend>Evaluation</legend>
                     <div class="container">
 
-                    <!-----------------------------
+                        <!-----------------------------
                     |       DateEval / PAR        |
                     ------------------------------>
                         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 mt-5">
                             <div class="col">
                                 <label for="libDate" class="form-label">Date d'Evaluation :</label>
-                                <input type="date" class="inputEval" id="inputEval" name="inputEval"
+                                <input type="text" class="inputEval" id="datepickerEval" name="inputEval"
                                     pattern="\d{1,2}/\d{1,2}/\d{4}" placeholder="jj/mm/aaaa">
                             </div>
 
@@ -49,26 +57,27 @@
                             </div>
                         </div>
 
-                    <!-----------------------------
+                        <!-----------------------------
                     |       Charge / Tarif        |
                     ------------------------------>
 
                         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 mt-4">
                             <div class="col mt-2">
                                 <label for="libCharge" class="form-label">Charge :</label>
-                                <input type="date" class="inputCharge" id="inputCharge" name="inputCharge"
-                                    pattern="\d{1,2}/\d{1,2}/\d{4}" placeholder="">
+                                <input type="text" class="inputCharge" id="datepickerCharge" name="inputCharge"
+                                    pattern="\d{1,2}/\d{1,2}/\d{4}" placeholder="jj/mm/aaaa">
                             </div>
 
                             <div class="col mt-2">
                                 <label for="libTarif" id="libTarif" class="form-label">Tarif :</label>
                                 <select id="selectTarif" name="selectTarif" class="form-select">
                                     <option value=''></option>
+                                    <option value=''>Tarif Octobre 2019</option>
                                 </select>
                             </div>
                         </div>
 
-                    <!--------------------------------
+                        <!--------------------------------
                     |       AccepteDate / PAR        |
                     --------------------------------->
 
@@ -80,70 +89,115 @@
 
                             <div class="col" id="accepteLe">
                                 <label for="accepteLe">le :</label>
-                                <input type="date" class="form-control" id="inputDateLe" name="datepicker"
-                                    pattern="\d{1,2}/\d{1,2}/\d{4}" placeholder="">
+                                <input type="text" class="inputDateLe" id="datepickerLe" name="datepicker"
+                                    pattern="\d{1,2}/\d{1,2}/\d{4}" placeholder="jj/mm/aaaa">
                             </div>
 
                             <div class="col" id="TarifPar">
                                 <label for="libTarifPar" id="libTarifPar" class="form-label">Par :</label>
                                 <select id="selectUtilTarif" name="selectUtilTarif" class="form-select">
                                     <option value=''></option>
+                                    <option value=''>ROUY</option>
                                 </select>
                             </div>
                         </div>
 
-                    <!---------------------------------
+                        <!---------------------------------
                     |       NbrJoursPassé / RAP        |
                     ----------------------------------->
 
-                        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-6 d-flex mt-5">
-                            <div class="col d-inline-flex">
-                                <label for="" class="form-label">Nombre de jours passés :</label>
-                                <input type="text" class="form-control" id="NbrJpasse" name="NbrJpasse" value="">
-                            </div>
+                        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-6 d-flex mt-5 text-center"
+                            id="RAP-NBJ-content">
 
-                            <div class="col d-inline-flex">
-                                <label for="" class="form-label">Reste à Passer : </label>
-                                <input type="text" class="form-control" id="ResteApasser" name="ResteApasser" value="">
-                            </div>
+                            <label for="" class="form-label">Nombre de jours passés :</label>
+                            <input type="text" class="form-control" id="NbrJpasse" name="NbrJpasse" value="">
+
+                            <label for="" class="form-label" id="libRAP">Reste à Passer : </label>
+                            <input type="text" class="form-control" id="ResteApasser" name="ResteApasser" value="">
                         </div>
 
-                    <!-------------------------------------------
+                        <!-------------------------------------------
                     |       ImpressionDevis / DétailTâche        |
                     --------------------------------------------->
-
-                        <!-- <div class="row row-cols-sm-2 row-cols-lg-2 display-flex mt-5"> -->
-                            <!-- <div class="col col-2">
-                                <button type="button" class="btn btn-outline-success" id="btnImpression">Impression
-                                    Devis</button>
+                        <div class="containerBtn">
+                            <div class="GroupBtn">
+                                <div class="devisBtn">
+                                    <button type="button" class="btn btn-outline-success" id="btnImpression">Impression
+                                        Devis</button>
+                                </div>
+                                <div class="detailBtn">
+                                    <button type="button" class="btn btn-outline-success" id="btnDetail">Détail de la
+                                        tâche</button>
+                                </div>
                             </div>
-
-                            <div class="col">
-                                <button type="button" class="btn btn-outline-success" id="btnDetail">Détail de la
-                                    tâche</button>
+                        </div>
+                        <!-------------------------------------------
+                    |       DEMANDE FACTURE / ACCES FACTUR      |
+                    -------------------------------------------->
+                        <div class="containerFacture">
+                            <div class="facture-content">
+                                <label for="libFacture" class="form-check-label">Demande Facturée</label>
+                                <input type="checkbox" class="form-check-input" id="checkFacture" value="">
+                                <input type="text" class="form-control" id="" name="" value="">
+                                <button type="button" class="btn btn-primary" id="btnFacture">Accès Facture</button>
                             </div>
-                        </div> -->
-
+                        </div>
                     </div>
                 </fieldset>
             </form>
         </section>
     </main>
 
-    <!-- LIEN BOOTSTRAP POPPERS -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    <!-- Autres scripts JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
         crossorigin="anonymous"></script>
 
-    <!-- LIEN BOOTSTRAP JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 
-    <!-- LIEN JQUERY -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker//1.4.1/locales/bootstrap-datepicker.fr.min.js"
+        integrity="sha512-ucy8JggPcTxFRqY6Sqz4lV6CBEmX4sLY7Pj7OBRXMCQnZW1XlMQLuGWd9UyTbkmW/A70lZ3nCfAdlWq1mFTrzA=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <script src="js/date.js"></script>
+
+
+
+    <!-- Votre script pour initialiser le datepicker -->
+    <script>
+        $(function () {
+            (function ($) { $.fn.datepicker.dates['fr'] = { days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"], daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam."], daysMin: ["di", "lu", "ma", "me", "je", "ve", "sa"], months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"], monthsShort: ["janv.", "févr.", "mars", "avril", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."], today: "Aujourd'hui", monthsTitle: "Mois", clear: "Effacer", weekStart: 1, format: "dd/mm/yyyy" }; }(jQuery));
+            $('#datepickerEval').datepicker({
+                autoclose: true,
+                todayHighlight: true,
+                language: 'fr',
+                format: 'dd/mm/yyyy',
+            });
+
+            $('#datepickerCharge').datepicker({
+                autoclose: true,
+                todayHighlight: true,
+                language: 'fr',
+                format: 'dd/mm/yyyy',
+            });
+
+            $('#datepickerCharge').datepicker({
+                autoclose: true,
+                todayHighlight: true,
+                language: 'fr',
+                format: 'dd/mm/yyyy',
+            });
+
+            $('#datepickerLe').datepicker({
+                autoclose: true,
+                todayHighlight: true,
+                language: 'fr',
+                format: 'dd/mm/aaaa'
+            });
+        });
+    </script>
+
 </body>
 
 </html>
